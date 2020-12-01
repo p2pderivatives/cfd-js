587ea9e98b10937655a88c02bd53d63a7bbe6cc0
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2020-12-01 14:51:49 +0900

    update from cryptogarageinc v0.2.11

--- tools/cmake_cleanup.bat
+++ tools/cmake_cleanup.bat
@@ -5,11 +5,9 @@ if exist "cmake_cleanup.bat" (
   cd ..
 )
 
-rmdir /S /Q wrap_js\build
+rmdir /S /Q build
 
 rmdir /S /Q wrap_js\dl_lib
-
-rmdir /S /Q wrap_js\wally.js
 
 rmdir /S /Q external\cfd
 
@@ -17,3 +15,4 @@ rmdir /S /Q external\cfd-core
 
 rmdir /S /Q external\libwally-core
 
+rmdir /S /Q external\googletest
